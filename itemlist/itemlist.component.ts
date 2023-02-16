3b036b954e09c9182a5b1ef58eead20ccd83afb8
r100spiv <123078998+r100spiv@users.noreply.github.com>
2023-02-16 15:43:22 +0200

    Add files via upload

---
+++ itemlist/itemlist.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-itemlist',
+  templateUrl: './itemlist.component.html',
+  styleUrls: ['./itemlist.component.css'],
+
+ 
+})
+export class ItemlistComponent implements OnInit {
+
+s1='If you think your teacher is tough, wait till you get a boss'
+s2=' Life is not fair — get used to it!'
+s3='Success is a lousy teacher. It seduces smart people into thinking they can’t lose'
+s4='Be nice to nerds. Chances are you’ll end up working for one'
+
+
+constructor(){}
+ngOnInit(){}
+
+}
